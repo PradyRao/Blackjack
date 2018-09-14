625ac779ea906c99eda34c350920c45637d0434f
Pradyumna Rao <pradyraoofficial@gmail.com>
2018-09-14 17:11:51 -0400

    added main class for card main: Card class implemented modify: slightly modified test cases for Card due to spelling error test: test running for Card without any errors or failures

---
+++ src/main/java/core/Card.java
@@ -0,0 +1,33 @@
+package core;
+
+public class Card {
+	String cardType;
+	String cardKey;
+	int cardValue;
+	
+	public Card(String type, String key, int value) {
+		cardType = type;
+		cardKey = key;
+		cardValue = value;
+	}
+	
+	public String getType() {
+		return cardType;
+	}
+	public String getKey() {
+		return cardKey;
+	}
+	public int getValue() {
+		return cardValue;
+	}
+	
+	public void setType(String type) {
+		cardType = type;
+	}
+	public void setKey(String key) {
+		cardKey = key;
+	}
+	public void setValue(int value) {
+		cardValue = value;
+	}
+}
--- src/test/java/core/Card_Test.java
+++ src/test/java/core/Card_Test.java
@@ -9,13 +9,13 @@ public class Card_Test extends TestCase{
 		//card with type, value type, and value
 		Card card = new Card("S", "5", 5);
 		
-		assertEqual("S", card.getType());
-		assertEqual("5", card.getKey());
-		assertEqual(5, card.getValue());
+		assertEquals("S", card.getType());
+		assertEquals("5", card.getKey());
+		assertEquals(5, card.getValue());
 		
 		card = new Card("C", "K", 10);
-		assertEqual("C", card.getType());
-		assertEqual("K", card.getKey());
-		assertEqual(10, card.getValue());
+		assertEquals("C", card.getType());
+		assertEquals("K", card.getKey());
+		assertEquals(10, card.getValue());
 	}
 }
